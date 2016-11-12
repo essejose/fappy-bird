7dbe4093d0f390115e3ae96ba5b502dc378af124
essejose <ze_pk2@hotmail.com>
2016-11-12 13:38:38 -0200

    create control

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdScript : MonoBehaviour {
+
+    public static BirdScript instance;
+
+    [SerializeField]
+    private Rigidbody2D myRigiBody;
+    [SerializeField]
+    private Animator anim;
+    
+    private float forwardSpeed = 3f;
+ 
+    private float bounceSpeed = 4f;
+
+    private bool didFlap;
+
+    public bool isAlive;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        isAlive = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (isAlive)
+        {
+            Vector2 temp = transform.position;
+            temp.x += forwardSpeed * Time.deltaTime;
+            transform.position = temp;
+            if (didFlap)
+            {
+                didFlap = false;
+                myRigiBody.velocity = new Vector2(0, bounceSpeed);
+                anim.SetTrigger("Flap");
+            }
+        }
+	}
+
+    public void FlapTheBird()
+    {
+        didFlap = true;
+    }
+}
