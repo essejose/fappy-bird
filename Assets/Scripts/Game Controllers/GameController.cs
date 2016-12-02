12e179fd83c43abb41ac8b38e63056a78b4f2b49
essejose <ze_pk2@hotmail.com>
2016-12-01 23:25:13 -0200

    Working in Game controller

--- Assets/Scripts/Game Controllers/GameController.cs
+++ Assets/Scripts/Game Controllers/GameController.cs
@@ -3,8 +3,22 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public static GameController instance;
+
+    private const string HIGH_SCORE = "Hight Score";
+    private const string SELECTED_BIRD = "Selected Bird";
+    private const string GREEN_BIRD  = "Green Bird";
+    private const string RED_BIRD = "Red Bird";
+    private const string BLUE_BIRD = "Blue Bird";
+
+
+    void Awake()
+    {
+        MakeSingleton();
+
+    }
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
@@ -12,4 +26,16 @@ public class GameController : MonoBehaviour {
 	void Update () {
 	
 	}
+    void MakeSingleton()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 }
